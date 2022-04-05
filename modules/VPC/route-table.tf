cceed8740475b2eee48c4280ae441c83c5843d4a
akingo <akinmoyerogabriel@gmail.com>
2022-04-06 00:39:30 +0100

    Update private route

--- modules/VPC/route-table.tf
+++ modules/VPC/route-table.tf
@@ -1,4 +1,4 @@
-
+#---Private Route Table---
 resource "aws_route_table" "private_route_table" {
   vpc_id = aws_vpc.vpc.id
   tags = merge(
@@ -10,11 +10,20 @@ resource "aws_route_table" "private_route_table" {
 
 }
 
+resource "aws_route" "private_route" {
+  route_table_id         = aws_route_table.private_route_table.id
+  nat_gateway_id         = aws_nat_gateway.nat_gateway.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
 resource "aws_route_table_association" "private_rta" {
   count          = var.preferred_number_of_private_subnets
   subnet_id      = element(aws_subnet.private.*.id, count.index)
   route_table_id = aws_route_table.private_route_table.id
 }
+
+
+#---Public Route Table---#
 
 
 resource "aws_route_table" "public_route_table" {
@@ -28,15 +37,8 @@ resource "aws_route_table" "public_route_table" {
 
 }
 
-
-resource "aws_route" "private_route" {
-  route_table_id         = aws_route_table.public_route_table.id
-  nat_gateway_id         = aws_nat_gateway.nat_gateway.id
-  destination_cidr_block = "0.0.0.0/0"
-}
-
 resource "aws_route" "public_route" {
-  route_table_id         = aws_route_table.private_route_table.id
+  route_table_id         = aws_route_table.public_route_table.id
   gateway_id             = aws_internet_gateway.ig.id
   destination_cidr_block = "0.0.0.0/0"
 }
